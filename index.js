77e2fb0af6c1edc0ad29f41ae70a0e95e11d84df
Alicia Weenum <weenum.alicia@gmail.com>
2021-01-27 13:22:54 -0500

    commit tree e7b6b2edeca5d0e626e10f7b5ba23d216c670969

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(names, event){
+    let cards = []
+    for (let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+return cards;
+}
+
+function countDown( num ) {
+    while ( num > 0 ) {
+      console.log( num );
+      num -= 1;
+    }
+    console.log( num );
+  }
